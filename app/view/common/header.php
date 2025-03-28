d9f56c57eddccf0cbca9e428cb45565f09c787ef
Emmanuelle-nsossani <emmanuelle.nsossani@outlook.com>
2025-03-29 00:50:52 +0100

    header update

--- app/view/common/header.php
+++ app/view/common/header.php
@@ -4,13 +4,55 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $page_title?></title>
+    <title><?= $page_title ?></title>
     <link rel="stylesheet" href="app/public/css/<?= $css ?>">
     <link rel="icon" href="favicon.ico" />
 </head>
 
 <body>
     <header>
-        HEADER
-        <!-- HEADER HERE -->
+        <div id="first-header">
+            <div class="first-header-sous">
+                <!-- From Uiverse.io by Madflows -->
+                <div class="toggle-switch">
+                    <label class="switch-label">
+                        <input type="checkbox" class="checkbox">
+                        <span class="slider"></span>
+                    </label>
+                </div>
+                <div id="language-menu">
+                    <p>
+                        <a href="">EN</a> | <a href="">FR</a> | <a href="">BUL</a> | <a href="">VIET</a>
+                    </p>
+                </div>
+            </div>
+            <a href="index.php">
+                <img id="logo-header" src="app/public/images/header-footer/logo-ice-crimes-white.svg" alt="logo Ice Cream">
+            </a>
+            <div class="first-header-sous">
+                <a class="link-button" href="">S'abonner</a>
+                <a href="">
+                    <img src="app/public/images/header-footer/person.svg" alt="">
+                </a>
+            </div>
+        </div>
+        <div id="second-header">
+            <nav>
+                <ul>
+                    <li><a href="">Préventions</a></li>
+                    <li><a href="">Rubriques &#10095</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li>
+                        <form action="">
+                            <button>
+                                <svg width="17" height="16" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-labelledby="search">
+                                    <path d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9" stroke="currentColor" stroke-width="1.333" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                            </button>
+                            <input type="text" placeholder="Search.." name="search">
+                        </form>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </header>
