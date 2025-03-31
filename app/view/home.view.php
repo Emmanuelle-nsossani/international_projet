9c1f844221ecb352ff991804c6662dc646df6d44
Emmanuelle-nsossani <emmanuelle.nsossani@outlook.com>
2025-04-01 00:57:51 +0200

    home page

--- app/view/home.view.php
+++ app/view/home.view.php
@@ -1,1 +1,17 @@
-<?= translateText("HOME PAGE EXAMPLE", $lang); ?>
+<main id="home-page">
+    <div id="presentation">
+        <div id="presentation-content">
+            <h1>Ice Crime<br>Le meilleur du crime</h1>
+            <p>
+                Notre plateforme vous plonge au cœur des faits divers et des affaires criminelles. Restez informé des tendances, analysez les événements marquants et accédez à des ressources pour mieux appréhender la réalité qui nous entoure. <br><br>
+                <b>Connectez-vous pour personnaliser votre expérience, suivre vos affaires favorites et recevoir des alertes adaptées à vos préférences.</b><br><br>
+                Le crime ne dort jamais… Savez-vous vraiment ce qui se passe dans ce monde ?
+            </p>
+        </div>
+        <img id="background-image" src="app/public/images/paper-header.png" alt="">
+    </div>
+    <div id="article">
+        
+    </div>
+
+</main>
---
+++ app/view/preferences.view.php
@@ -0,0 +1,14 @@
+  	<div class="preference">
+  		<h1>Mes préférences</h1>
+		<div id="preference-container">
+			<div class="preference-item">
+				<h2>Your favorite category</h2>
+			</div>
+			<div class="preference-item">
+
+			</div>
+			<div class="preference-item">
+				
+			</div>
+		</div>
+  	</div>
