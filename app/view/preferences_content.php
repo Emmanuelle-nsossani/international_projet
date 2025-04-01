7a74147cfb1143c082315e7035be963b4525650e
Emmanuelle-nsossani <emmanuelle.nsossani@outlook.com>
2025-04-01 11:59:49 +0200

    resolve bug preference

---
+++ app/view/preferences_content.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="initial-scale=1, width=device-width">
+  	
+ 
+  	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap" />
+  	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Actor:wght@400&display=swap" />
+  	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=ABeeZee:wght@400&display=swap" />
+  	
+  	
+  	
+</head>
+<body>
+  	
+  	<div class="prfrences">
+			<span class="text-7">Mes préférences</span>
+		<div class="wrapper-3">
+		<div class="box-3">
+		<div class="img-3"><span class="text-8">Categorie 1</span></div>
+		<div class="pic-5"><span class="text-9">Categorie 1</span></div>
+		<div class="img-4"><span class="text-a">Categorie 1</span></div>
+		<div class="pic-6"><span class="text-b">Categorie 1</span></div>
+		</div>
+		<div class="group-3">
+		<div class="pic-7"><span class="text-c">Categorie 1</span></div>
+		<div class="pic-8"><span class="text-d">Categorie 1</span></div>
+		<div class="img-5"><span class="text-e">Categorie 1</span></div>
+		<div class="pic-9"><span class="text-f">Categorie 1</span></div>
+		</div>
+		<div class="pic-a"><span class="text-10">Categorie 1</span></div>
+		<div class="section">
+		<div class="pic-b">
+		<span class="text-11">Toutes les heures</span>
+		</div>
+		<div class="img-6">
+		<span class="text-12">1 fois par jour</span>
+		</div>
+		<div class="img-7"></div>
+		<span class="text-13">1 fois par semaine</span>
+		</div>
+		<div class="wrapper-4">
+		<div class="img-8"><span class="text-14">Français</span></div>
+		<div class="pic-c"><span class="text-15">English</span></div>
+		<div class="pic-d"><span class="text-16">Vietnam</span></div>
+		<div class="img-9"><span class="text-17">Bulgarien</span></div>
+		</div>
+		<div class="pic-e"><span class="text-18">Enrengistrer</span></div>
+		</div>
+  	</div>
+</html>
