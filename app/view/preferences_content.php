8d47639b8004e0671dba50f988e90162a1081f0c
Nguyen Thi Nhung <nhunghp301279@gmail.com>
2025-04-01 16:40:04 +0700

    Fixed the footer logo and preferences page

--- app/view/preferences_content.php
+++ app/view/preferences_content.php
@@ -4,7 +4,7 @@
   	<meta charset="utf-8">
   	<meta name="viewport" content="initial-scale=1, width=device-width">
   	
-  	<link rel="stylesheet"  href="./index.css" />
+ 
   	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap" />
   	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Actor:wght@400&display=swap" />
   	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=ABeeZee:wght@400&display=swap" />
@@ -15,78 +15,38 @@
 <body>
   	
   	<div class="prfrences">
-    		<h1 class="mes-prfrences">Mes préférences</h1>
-    		<div class="prfrences-child">
-          <div class="group-parent">
-      			<div class="vector-parent">
-        				<img class="vector-icon1" alt="" src="app\public\images\Vector2.svg">
-        				<div class="toutes-les-heures">Categorie 1</div>
-      			</div>
-      			<div class="vector-group">
-        				<img class="vector-icon1" alt="" src="app\public\images\Vector2.svg">
-        				<div class="franais">Français</div>
-      			</div>
-      			<div class="vector-container">
-        				<img class="vector-icon1" alt="" src="app\public\images\Vector4.svg">
-        				<div class="toutes-les-heures">Toutes les heures</div>
-      			</div>
-      			<div class="group-div">
-        				<img class="vector-icon1" alt="" src="app\public\images\Vector6.svg">
-        				<div class="toutes-les-heures">1 fois par jour</div>
-      			</div>
-      			<div class="vector-parent1">
-        				<img class="vector-icon5" alt="" src="app\public\images\Vector5.svg">
-        				<div class="fois-par-semaine">1 fois par semaine</div>
-      			</div>
-      			<div class="vector-parent2">
-        				<img class="vector-icon1" alt="" src="app\public\images\Vector2.svg">
-        				<div class="toutes-les-heures">Categorie 1</div>
-      			</div>
-      			<div class="vector-parent3">
-        				<img class="vector-icon1" alt="" src="app\public\images\Vector2.svg">
-        				<div class="toutes-les-heures">Categorie 1</div>
-      			</div>
-      			<div class="vector-parent4">
-        				<img class="vector-icon1" alt="" src="app\public\images\Vector2.svg">
-        				<div class="english">English</div>
-      			</div>
-      			<div class="vector-parent5">
-        				<img class="vector-icon1" alt="" src="app\public\images\Vector3.svg">
-        				<div class="enrengistrer">Enrengistrer</div>
-      			</div>
-      			<div class="vector-parent6">
-        				<img class="vector-icon1" alt="" src="app\public\images\Vector2.svg">
-        				<div class="toutes-les-heures">Categorie 1</div>
-      			</div>
-      			<div class="vector-parent7">
-        				<img class="vector-icon1" alt="" src="app\public\images\Vector2.svg">
-        				<div class="toutes-les-heures">Categorie 1</div>
-      			</div>
-      			<div class="vector-parent8">
-        				<img class="vector-icon1" alt="" src="app\public\images\Vector2.svg">
-        				<div class="vietnam">Vietnam</div>
-      			</div>
-      			<div class="vector-parent9">
-        				<img class="vector-icon1" alt="" src="app\public\images\Vector2.svg">
-        				<div class="toutes-les-heures">Categorie 1</div>
-      			</div>
-      			<div class="vector-parent10">
-        				<img class="vector-icon1" alt="" src="app\public\images\Vector2.svg">
-        				<div class="toutes-les-heures">Categorie 1</div>
-      			</div>
-      			<div class="vector-parent11">
-        				<img class="vector-icon1" alt="" src="app\public\images\Vector2.svg">
-        				<div class="toutes-les-heures">Bulgarien</div>
-      			</div>
-      			<div class="vector-parent12">
-        				<img class="vector-icon1" alt="" src="app\public\images\Vector2.svg">
-        				<div class="toutes-les-heures">Categorie 1</div>
-      			</div>
-      			<div class="vector-parent13">
-        				<img class="vector-icon1" alt="" src="app\public\images\Vector2.svg">
-        				<div class="toutes-les-heures">Categorie 1</div>
-      			</div>
-          </div>
-    		</div>
+			<span class="text-7">Mes préférences</span>
+		<div class="wrapper-3">
+		<div class="box-3">
+		<div class="img-3"><span class="text-8">Categorie 1</span></div>
+		<div class="pic-5"><span class="text-9">Categorie 1</span></div>
+		<div class="img-4"><span class="text-a">Categorie 1</span></div>
+		<div class="pic-6"><span class="text-b">Categorie 1</span></div>
+		</div>
+		<div class="group-3">
+		<div class="pic-7"><span class="text-c">Categorie 1</span></div>
+		<div class="pic-8"><span class="text-d">Categorie 1</span></div>
+		<div class="img-5"><span class="text-e">Categorie 1</span></div>
+		<div class="pic-9"><span class="text-f">Categorie 1</span></div>
+		</div>
+		<div class="pic-a"><span class="text-10">Categorie 1</span></div>
+		<div class="section">
+		<div class="pic-b">
+		<span class="text-11">Toutes les heures</span>
+		</div>
+		<div class="img-6">
+		<span class="text-12">1 fois par jour</span>
+		</div>
+		<div class="img-7"></div>
+		<span class="text-13">1 fois par semaine</span>
+		</div>
+		<div class="wrapper-4">
+		<div class="img-8"><span class="text-14">Français</span></div>
+		<div class="pic-c"><span class="text-15">English</span></div>
+		<div class="pic-d"><span class="text-16">Vietnam</span></div>
+		<div class="img-9"><span class="text-17">Bulgarien</span></div>
+		</div>
+		<div class="pic-e"><span class="text-18">Enrengistrer</span></div>
+		</div>
   	</div>
 </html>
